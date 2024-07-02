ae815f390f5f8ce0483445c76b3a79d39311924d
jatsc104 <jann@tschierschke.org>
2024-07-02 23:41:52 +0200

    handle all rx related interrupts

--- os/kernel/src/device/e1000_interrupt.rs
+++ os/kernel/src/device/e1000_interrupt.rs
@@ -1,22 +1,30 @@
 use alloc::boxed::Box;
+use alloc::vec::Vec;
 use log::info;
 use pci_types::InterruptLine;
 use x86_64::registers;
 
+use crate::device::e1000_descriptor::{retrieve_packets, rx_ring_pop, E1000RxDescriptor};
 use crate::interrupt::interrupt_handler::InterruptHandler;
 use crate::interrupt::interrupt_dispatcher::{InterruptVector};
 use crate::{apic, interrupt, interrupt_dispatcher};
 use crate::device::e1000_register::E1000Registers;
+use crate::device::e1000_driver::{IntelE1000Device, RxBufferVecToPtr, RxRingVecToPtr};
+use crate::alloc::rc::Rc;
 
 struct E1000InterruptHandler{
     registers: E1000Registers,
+    rx_ring: Vec<E1000RxDescriptor>,
+    rx_buffer: Vec<Vec<u8>>,
 }
 
 //seperate impl block needed, since new is not part of InterruptHandler trait
 impl E1000InterruptHandler{
-    fn new(registers:E1000Registers) -> Self{
+    fn new(registers: E1000Registers, rx_desc_ring: Vec<E1000RxDescriptor>, received_buffer: Vec<Vec<u8>>) -> Self{
         E1000InterruptHandler{
             registers,
+            rx_ring: rx_desc_ring,
+            rx_buffer: received_buffer,
         }
     }
 }
@@ -36,6 +44,11 @@ impl InterruptHandler for E1000InterruptHandler{
         const ICR_TXQE: u32 = 1 << 1;
         const ICR_LSC: u32 = 1 << 2;
         const STATUS_LU: u32 = 1 << 1;
+        const ICR_RXDMT0: u32 = 1 << 4;
+        //bit 5 is reserved
+        const ICR_RXO: u32 = 1 << 6;
+        const ICR_RXT0: u32 = 1 << 7;
+        //bit 8 is reserved
         //clearing unnecessary - see above
         //self.registers.write_icr(interrupt_cause & !(ICR_TXDW) & !(ICR_TXQE));
 
@@ -58,20 +71,42 @@ impl InterruptHandler for E1000InterruptHandler{
             }
         }
 
+        if interrupt_cause & (ICR_RXDMT0 | ICR_RXO) != 0{
+            info!("Receive Descriptor Minimum Threshold Reached or Receive Overrun");
 
-
-
+            retrieve_packets(&mut self.rx_ring, &self.registers, &mut self.rx_buffer);
 
         info!("Interrupt handled");
+        }
+
+        if interrupt_cause & (ICR_RXT0) != 0{
+            //single packet received - data races between interrupt and new packet possible? maybe two packets received at the "same" time?
+            //data race technically handled by RXDMT0 and RXO, regular clearing of packets might be a good idea
+            //pop a sinlgle packet from rx_desc_ring. or actually loop starting at the tail up to head-1
+
+            rx_ring_pop(&mut self.rx_ring, &self.registers, &mut self.rx_buffer);
+
+        }
+
+        //UNHANDLED INTERRUPTS
+
+        //MDAC - MDI/O access complete - phy and ethernet controller connected
+        //RXCFG - used when forcing link - the latter not done by me
+        //PHYINT - phy interrupt
+        //GPI_SDP6 - general purpose interrupt
+        //GPI_SDP7 - general purpose interrupt
+        //GPI - general purpose interrupt
+        //TXD_LOW - transmit descriptor low threshold - should be implemented
+        //SRPD - small receive packet detected
+
     }
 }
 
 
-
-pub fn map_irq_to_vector(interrupt_line: InterruptLine, registers: E1000Registers){
+pub fn map_irq_to_vector(interrupt_line: InterruptLine, registers: E1000Registers, rx_desc_ring: Vec<E1000RxDescriptor>, received_buffer: Vec<Vec<u8>>){
     //add 32 because first 32 are reserved for cpu exceptions
     let interrupt_vector = InterruptVector::try_from(interrupt_line as u8 + 32).unwrap();
-    let handler = Box::new(E1000InterruptHandler::new(registers));
+    let handler = Box::new(E1000InterruptHandler::new(registers, rx_desc_ring, received_buffer));
     interrupt_dispatcher().assign(interrupt_vector, handler);
     apic().allow(interrupt_vector);
 }
