ef2af8e44a7ed64059350b32c885049fba8ab12b
jatsc104 <jann@tschierschke.org>
2024-07-11 17:30:47 +0200

    e1000 app now uses syscalls instead of kernel dependencies - not compiling

--- os/application/e1000/src/e1000.rs
+++ os/application/e1000/src/e1000.rs
@@ -1,9 +1,9 @@
 #![no_std]
 
 extern crate alloc;
-extern crate kernel;
 
 use alloc::vec::Vec;
+use syscall::{syscall1, syscall2};
 use kernel::e1000_device;
 use kernel::device::e1000_interface::{NetworkProtocol, transmit, receive_data};
 use kernel::device::e1000_driver::IntelE1000Device;
@@ -44,11 +44,22 @@ pub fn main() {
     let data_array: [u8; 64] = [0b01010101; 64];
     let mut data_vec = Vec::from(EthernetHeader.to_bytes().to_vec());
     data_vec.extend_from_slice(&data_array);
-    transmit(data_vec, NetworkProtocol::Ethernet, &mut device);
+//TODO: change transmit call to system call - change data_vec to &Vec<u8> and resolve NetworkProtocol enum in syscall
+        //furthermore, i have no access to the device, but since it will never change i can get it inside the syscall
+    let data_ptr = &data_vec as *const _ as usize;
+    syscall2(sys_transmit_data, data_ptr, 0 as usize);
+    //transmit(data_vec, NetworkProtocol::Ethernet, &mut device);
     println!("Data sent");
     //wait for the packet to be sent and received/put on the receive queue
-    Timer::wait(5000);
-    let received_data = receive_data(&mut device);
+    //timer is not accessible in the application, maybe just loop for quite some time
+    //Timer::wait(5000);
+    for _ in 0..500_000 {
+        // This loop does nothing but waste time.
+    }
+//TODO: change receive_data call to system call -> to return the data, try to give a &mut Vec<u8> as argument
+    let received_data: Vec<u8> = Vec::new();
+    let received_data_ptr = &received_data as *const _ as usize;
+    syscall1(sys_receive_data, received_data_ptr);
     match received_data{
         Some(data) => {
             println!("Received data: {:?}", data);
