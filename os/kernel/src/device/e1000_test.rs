89354e569b99350d1ac7083cad1135cc0d2bab93
jatsc104 <jann@tschierschke.org>
2024-08-01 16:24:16 +0200

    commented out not compiling dead code, improvements to fake lbm

--- os/kernel/src/device/e1000_test.rs
+++ os/kernel/src/device/e1000_test.rs
@@ -10,10 +10,10 @@ use super::e1000_register::E1000Registers;
 pub fn e1000_fake_test(){
     let device = e1000_device();
     let mac = device.mac_address;
-    let ethernet_header = build_ethernet_header(mac);
+//    let ethernet_header = build_ethernet_header(mac);
     let data_array: [u8; 64] = [0b01010101; 64];
-    let mut data_vec = Vec::from(ethernet_header.to_bytes().to_vec());
-    data_vec.extend_from_slice(&data_array);
+//    let mut data_vec = Vec::from(ethernet_header.to_bytes().to_vec());
+//    data_vec.extend_from_slice(&data_array);
     //fake_transmit(data_vec, device);
 }
 
@@ -36,7 +36,12 @@ pub fn fake_transmit_lbm(rx_ring: &mut Vec<E1000RxDescriptor>, registers: &E1000
     //combine tx_ring_low and tx_ring_high to get the address of the ring
     let tx_ring_address = (tx_ring_high as u64) << 32 | tx_ring_low as u64;
     //get the index of the descriptor that was sent
-    let sent_index = (tx_ring_tail - 1) % (tx_ring_len/16);
+//    let sent_index = (tx_ring_tail - 1) % (tx_ring_len/16);
+    let sent_index = if tx_ring_tail == 0 {
+        (tx_ring_len / 16) - 1
+    } else {
+        (tx_ring_tail - 1) % (tx_ring_len / 16)
+    };
     //get the descriptor that was sent
     let tx_ring_ptr = tx_ring_address as *mut E1000TxDescriptor;
     let sent_descriptor_ptr = unsafe { tx_ring_ptr.offset(sent_index as isize) };
@@ -63,7 +68,7 @@ pub fn fake_transmit_lbm(rx_ring: &mut Vec<E1000RxDescriptor>, registers: &E1000
             descriptor.length = sent_data_len as u16;
             descriptor.status = 1;
             //increment rx_ring_head
-            registers.write_rdh(((rx_ring_head + 1) % rdlen) as u32);
+            registers.write_rdh(((rx_ring_head + 1) % (rdlen/16)) as u32);
         }
         //increment i
         i += 1;
